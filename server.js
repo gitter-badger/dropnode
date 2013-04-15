44a48a223afb5a237d9334191e2b265d19652031
divanvisagie <visagiedivan@gmail.com>
2013-04-15 20:54:00 +0200

    stubs

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var mdns = require('mdns');
-var os = require( 'os' );
+var mdns = require('mdns'),
+	os = require( 'os' ),
+	narf = require( 'narf' ),
+	request = require( 'request' );
 
-// advertise a http server on port 4321
+// advertise a http server on relavant port
 var txt_record = {
 
 	name : 'dropnode'
@@ -39,3 +41,72 @@ browser.start();
 
 // discover all available service types
 var all_the_types = mdns.browseThemAll(); // all_the_types is just another browser...
+
+
+/* This is where the magic happens */
+
+var pullRequests = []; //requests the reciever must confirm
+
+var postRequests = []; //files the pusher has yet to push
+
+var serverFunctions = {
+
+	PUT : {
+
+		/* The sender of the file asks the receiver 
+		to retrieve it from them, the request is added to the
+		pullRequests list for confirmation */
+		pullRequest : function( data , callback ){
+
+			//TODO:
+			callback( { 'success' : 'target recieved request' } );
+		},
+
+		/* The sender cancels the pullRequest removing it 
+		from the list */
+		cancelRequest : function( data , callback ){
+
+			//TODO:
+			callback();
+		},
+
+		/* The receiver rejects a request and the request is removed 
+		from the postRequests list*/
+		rejectReqeust : function( data , callback ){
+
+			//TODO:
+			callback( { 'success' : 'recieved rejection' } );
+		}
+	},
+
+	GET : {
+
+		/* The receiver accepts the request and pulls the file */
+		acceptRequest : function( data, callback ){
+
+			//TODO:
+			callBack( { 'success' : 'transfer complete' } );
+		}
+	}
+};
+
+var clientFunctions = {
+
+	/* ask a receiver if it wants a file */
+	pullRequest : function(){
+
+		var method = 'PUT',
+			serverfunction = 'pullRequest';
+
+
+	}
+};
+
+var httpServer = new narf.HttpServer();
+httpServer.start( 'auto' );
+
+httpServer.on( 'port' , function( port ){
+
+	console.log( 'running narf on port' , port );
+
+} );
